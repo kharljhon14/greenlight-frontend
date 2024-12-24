e5efef1381956620ef1c33797a2aa02d9a7b1702
Kharl <enriquezkharl14@gmail.com>
2024-12-24 23:17:27 +0800

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from './components/ui/button';
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-red-200">Helloworld</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
